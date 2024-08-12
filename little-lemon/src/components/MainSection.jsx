7c213648a823c75499cdbb8609faecda45cf66c5
rahbhee <ahmedrabi2003@gmail.com>
2024-08-12 09:20:42 +0100

    second commit

---
+++ little-lemon/src/components/MainSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainSection = () =>{
+    return (
+        <header>
+            <nav></nav>
+            <section></section>
+        </header>
+    )
+}
+export default MainSection;
